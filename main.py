593d79261f644ddd1f7aedbfea6ade9294c6c01b
LucasTalen <lucaspaula58227@outlook.com>
2023-11-29 22:42:07 -0300

    add vez de jogador

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def criarSala(token,nomeSala):
         salas[codigo]['estado'] = ['-','-','-','-','-','-','-','-','-']
         salas[codigo]['jogador1'] = token
         salas[codigo]['jogador1-X_ou_O'] = 'X'
-        salas[codigo]['primeiro_jogador'] = '-'
+        salas[codigo]['ultima_jogada'] = '-'
     print(salas)
     return 'succes'
 
@@ -39,11 +39,36 @@ def mostrarSala(token):
 def fazerAcao(token,lista,acao):
 
     for codigo in salas:
-        
-        if salas[codigo]['jogador1'] == token:
-            vez = salas[codigo]['jogador1-X_ou_O']
-        elif salas[codigo]['jogador2'] == token:
-            vez = salas[codigo]['jogador2-X_ou_O']
+
+        if salas[codigo]['jogador1'] == token or salas[codigo]['jogador2'] == token:
+            if salas[codigo]['ultima_jogada'] == '-':
+                if salas[codigo]['jogador1'] == token:
+                    salas[codigo]['ultima_jogada'] = 'X'
+                    vez = salas[codigo]['jogador1-X_ou_O']  
+                elif salas[codigo]['jogador2'] == token:
+                    salas[codigo]['ultima_jogada'] = 'O'
+                    vez = salas[codigo]['jogador2-X_ou_O']  
+                
+            else:
+                if salas[codigo]['jogador1'] == token:
+                    if  salas[codigo]['jogador1-X_ou_O'] ==  salas[codigo]['ultima_jogada']:
+                        return False
+                    else:
+                        salas[codigo]['ultima_jogada'] = 'X'
+                        vez = salas[codigo]['jogador1-X_ou_O'] 
+                        
+            
+                elif salas[codigo]['jogador2'] == token:
+                    if  salas[codigo]['jogador2-X_ou_O'] ==  salas[codigo]['ultima_jogada']:
+                        return False
+                    else:
+                        salas[codigo]['ultima_jogada'] = 'O'
+                        vez = salas[codigo]['jogador2-X_ou_O'] 
+                
+
+
+
+
 
     lista1 = lista.split(',')
 
--- test.py
+++
@@ -1,8 +0,0 @@
-a = 1
-
-if b == 0:
-    b = 1
-else:
-    b = 0
-
-print(b)
